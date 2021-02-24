811163eef1ac345f1413fcb7512132d4885c030f
abelyani <64776261+abelyani@users.noreply.github.com>
2021-02-24 21:54:22 +0300

    Add files via upload

---
+++ StockFiller.py
@@ -0,0 +1,20 @@
+def FillStocks(df):
+    # importing pandas
+    import pandas as pd
+  
+    # r for range of correct date values
+    r = pd.date_range(start=df.index.min(), end=df.index.max())
+    
+    # reindexing the dates of the dataframe
+    df = df.reindex(r).rename_axis('Date')
+    
+    # counting the maximum number of subsequent NAN values in our stock dataframe
+    col = df.columns[0]
+    max_NANs = max(df[col].isnull().astype(int).groupby(df[col].notnull().astype(int).cumsum()).sum())
+    
+    # filling the NANs using 'pad' or 'ffill' A.K.A. 'feed values forward'
+    df = df.fillna(method='pad')
+    
+    # simple print and return
+    print("The highest number of subsequent NAN values was \""+str(max_NANs)+"\". If the nubmer is 3 or less than you're good to go!")
+    return df
